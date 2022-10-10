328826272ea5e1c8cd1514e5d78b8229a0414e05
Winvoker <bayhan2000@outlook.com>
2022-10-10 12:04:28 +0300

    do not add to database and flake 8 updates

--- bot.py
+++ bot.py
@@ -1,31 +1,20 @@
-from discord.ext import tasks, commands
+import aiocron
 import discord
+from discord.ext import tasks, commands
 from utils import print_menu, update_yemekhane, get_date, read_token, read_channelID
 from database import update_json, add_message, create_db, save_db
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
-ignored_channels = [
-    "928275152830103623",
-    "933765068183728180",
-    "934464950817132554",
-    "922518769459429396"
-]
 
-global db, db_bot
-
-db = create_db()
-db_bot = create_db()
-
-@tasks.loop(hours=24.0)
+@aiocron.crontab("0 12 * * *")
 async def auto_send():
     channel = await client.fetch_channel(read_channelID())
     date = get_date(1)
-    if date[0] == '0':
+    if date[0] == "0":
         date = date[1:]
-    
-    print(date)
+
     yemekhane = update_yemekhane()
     update_json(yemekhane)
 
@@ -34,19 +23,10 @@ async def auto_send():
 
     await channel.send(msg)
 
-@tasks.loop(hours=24.0)
-async def auto_save_db():
-    global db, db_bot
-    save_db(db)
-    db = create_db()
-
-    save_db(db_bot)
-    db_bot = create_db()
 
 @client.event
 async def on_ready():
     print(f"We have logged in as {client.user}")
-    auto_save_db.start()
     auto_send.start()
 
 
@@ -57,11 +37,6 @@ async def on_message(message):
     if message.author.bot == True:
         return
 
-    if message.channel.id not in ignored_channels:
-        add_message(db, message)
-    else:
-        add_message(db_bot, message)
-
     if message.content.startswith(".yemek"):
         if "yarın" in message.content:
             date = get_date(1)
@@ -70,9 +45,9 @@ async def on_message(message):
         else:
             date = get_date()
 
-        if date[0] == '0':
+        if date[0] == "0":
             date = date[1:]
-        
+
         print(message)
         print(date)
         yemekhane = update_yemekhane()
@@ -81,5 +56,6 @@ async def on_message(message):
         msg += print_menu(yemekhane[date])
 
         await message.channel.send(msg)
+
 
 client.run(read_token())
--- database.py
+++ database.py
@@ -6,20 +6,24 @@ from datetime import datetime
 def save_db(db):
     dt = datetime.now()
     ts = datetime.timestamp(dt)
-    with open(f"data/messages/{ts}.json", "w", encoding='utf-8') as f:
+    with open(f"data/messages/{ts}.json", "w", encoding="utf-8") as f:
         f.write(json.dumps(db, ensure_ascii=False))
 
+
 def create_db():
-    db = {'date':[], 'author' : [], 'channel': [], 'message' : []}
+    db = {"date": [], "author": [], "channel": [], "message": []}
     return db
 
+
 def load_json(yemekhane_json="yemekhane.json"):
-    with open("yemekhane.json", "r") as f:
+    with open("yemekhane.json", "r", encoding="utf-8") as f:
         return json.loads(f.read())
 
+
 def save_json(yemekhane_dict="yemekhane.json"):
-    with open("yemekhane.json", "w", encoding='utf-8') as f:
+    with open("yemekhane.json", "w", encoding="utf-8") as f:
         f.write(json.dumps(yemekhane_dict, ensure_ascii=False))
+
 
 def update_json(new_json):
     old_json = load_json()
@@ -28,7 +32,7 @@ def update_json(new_json):
 
 
 def add_message(db, message):
-    db['date'].append(message.created_at.__str__())
-    db['author'].append(message.author.__str__())
-    db['channel'].append(message.channel.__str__())
-    db['message'].append(message.content.__str__())
+    db["date"].append(message.created_at.__str__())
+    db["author"].append(message.author.__str__())
+    db["channel"].append(message.channel.__str__())
+    db["message"].append(message.content.__str__())
